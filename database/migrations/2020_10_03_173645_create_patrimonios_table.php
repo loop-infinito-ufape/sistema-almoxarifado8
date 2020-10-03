f267f65e59b752e411944c0a36d4253437480a6b
pirangabaneto <pirangabaneto@gmail.com>
2020-10-03 14:38:06 -0300

    criando model e migration de Patrimonio

---
+++ database/migrations/2020_10_03_173645_create_patrimonios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatrimoniosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patrimonios', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero');
+            $table->integer('status');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patrimonios');
+    }
+}
