fdad04978662f9b850a1b1bbb8abeb12f7037a7e
pirangabaneto <pirangabaneto@gmail.com>
2020-10-03 14:36:30 -0300

    criando model e migration de Pedido

---
+++ database/migrations/2020_10_03_170723_create_pedidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao')->nullable();
+            $table->integer('quantidade_pedida');
+            $table->integer('quantidade_despachadada');
+            $table->dateTime('data_inicial');
+            $table->dateTime('data_final')->nullable();
+            $table->integer('status');
+            $table->string('observacao')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
