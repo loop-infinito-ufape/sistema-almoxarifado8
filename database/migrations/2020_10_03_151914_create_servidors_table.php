3a992e5328db756fa7626d53d4e801eccd717b60
pirangabaneto <pirangabaneto@gmail.com>
2020-10-03 14:01:19 -0300

    criando model e migration de servidor

---
+++ database/migrations/2020_10_03_151914_create_servidors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServidorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servidors', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('telefone');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('if');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servidors');
+    }
+}
