8e025566fcf668514f0191895dd1a7593b6e7098
pirangabaneto <pirangabaneto@gmail.com>
2020-10-04 01:32:55 -0300

    adicionando validacao de funcionario

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cadastrar', [\App\Http\Controllers\FuncionarioController::class, 'prepararCadastro']);
+Route::post('/cadastrar', [\App\Http\Controllers\FuncionarioController::class, 'cadastrar'])->name('funcionario.criar');
+
