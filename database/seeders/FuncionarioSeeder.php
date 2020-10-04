d5a337c466b1ca4d1a0fc0869d714cb131f963dc
GuilhermeSilva99 <guiobmep@gmail.com>
2020-10-04 01:19:59 -0300

    Criando o seeder de funcionario

---
+++ database/seeders/FuncionarioSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Funcionario;
+use Illuminate\Database\Seeder;
+
+class FuncionarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Funcionario::factory()->count(10)->create();
+    }
+}
