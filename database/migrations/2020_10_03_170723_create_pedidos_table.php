1c196e4bac40cdb24114c3b39e4a59093c01f428
pirangabaneto <pirangabaneto@gmail.com>
2020-10-04 17:26:27 -0300

    adicionando nullable ao atributo quantidade_despachada

--- database/migrations/2020_10_03_170723_create_pedidos_table.php
+++ database/migrations/2020_10_03_170723_create_pedidos_table.php
@@ -17,17 +17,17 @@ class CreatePedidosTable extends Migration
             $table->id();
             $table->string('descricao')->nullable();
             $table->integer('quantidade_pedida');
-            $table->integer('quantidade_despachadada');
+            $table->integer('quantidade_despachadada')->nullable();
             $table->dateTime('data_inicial');
             $table->dateTime('data_final')->nullable();
             $table->integer('status');
             $table->string('observacao')->nullable();
-            
+
             $table->integer('funcionario_id')->unsigned()->nullable();
             $table->integer('servidor_id')->unsigned();
             $table->integer('tipo_equipamento_id')->unsigned();
             $table->integer('pedido_anterior_id')->unsigned()->nullable();
-            
+
             $table->foreign('funcionario_id')->references('id')->on('funcionarios');
             $table->foreign('servidor_id')->references('id')->on('servidors');
             $table->foreign('tipo_equipamento_id')->references('id')->on('tipo_equipamentos');
