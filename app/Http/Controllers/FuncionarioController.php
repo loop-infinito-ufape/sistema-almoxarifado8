5e668a9613845e0a91495c78ae870943fbc50b7b
pirangabaneto <pirangabaneto@gmail.com>
2020-10-08 08:05:46 -0300

    implementando edicao de funcionario

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -3,28 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Models\Funcionario;
+use App\Models\User;
 use App\Validator\FuncionarioValidator;
 use App\Validator\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class FuncionarioController extends Controller
 {
-    //carregar a pagina
-    public function prepararCadastro(){
-        return view ("formCadastrarFuncionario");
+    public function prepararEditar(){
+        $user = User::find(Auth::user()->id);
+        $funcionario = Funcionario::where('user_id', $user->id)->first();
+        return view('formEditarFuncionario',['user' => $user, 'funcionario' => $funcionario]);
     }
-
-    //cadastrar o usuario
-    public function cadastrar(Request $request){
+    public function editar(Request $request){
+        $user = Auth::user();
+        $funcionario = Funcionario::where('user_id', $user->id)->first();
         try {
+            //caso o funcionario tenha mudado o email
+            if($user->email != $request->email){
+               $request->validate(['email' => ['required', 'string', 'email', 'max:255', 'unique:users']],['email.unique' => "Email já registrado.",
+        'email.*' => "Email inválido."]);
+            }
+
             FuncionarioValidator::validate($request->all());
-            $dados = $request->all();
-            $dados['senha'] = Hash::make($dados['senha']);
-            Funcionario::create($dados);
-            return "Funcionario criado";
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->telefone = $request->telefone;
+            $user->save();
+
+            $funcionario->cpf = $request->cpf;
+            $funcionario->save();
+
+            return redirect(route('home'))
+                ->with('message', 'works');
         }catch (ValidationException $exception){
-            return redirect(route('funcionario.criar') )
+           // dd($exception);
+            return redirect(route('funcionario.editar') )
                 ->withErrors($exception->getValidator())
                 ->withInput();
         }
---
+++ resources/views/formEditarFuncionario.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Editar Perfil') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('funcionario.editar') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$user->name }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Telefone') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="telefone" type="text" class="form-control @error('telefone') is-invalid @enderror" name="telefone" value="{{ $user->telefone}}" required autocomplete="telefone" autofocus>
+
+                                @error('telefone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('CPF') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="cpf" type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" value="{{ $funcionario->cpf}}" required autocomplete="cpf" autofocus>
+
+                                @error('cpf')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
