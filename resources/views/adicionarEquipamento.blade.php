8d736e92bddb1c45cf73427ea60ec78c5c137a66
GuilhermeSilva99 <guiobmep@gmail.com>
2020-10-27 18:20:38 -0300

    Reorganizacao dos formularios da pagina

--- resources/views/adicionarEquipamento.blade.php
+++ resources/views/adicionarEquipamento.blade.php
@@ -8,87 +8,89 @@
                     <div class="card-header">{{ __('Adicionar equipamento') }}</div>
 
                     <div class="card-body">
-                        <form id="form1" method="POST" action="{{ route('adicionar') }}">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Equipamento') }}</label>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <form id="form1" method="POST" action="{{ route('adicionar') }}">
+                                    @csrf
+                                    <div class="form-group row">
+                                        <div class="col-md-12">
+                                            <label for="name">{{ __('Equipamento') }}</label>
 
-                                <div class="col-md-6">
+                                            <select id="tipo_id"  class="form-control" name="tipo_id" autocomplete="tipo_id" autofocus>
+                                                <option value="">Indefinido</option>
+                                                @foreach($tipos as $tipo)
+                                                    <option value="{{$tipo->id}}">{{$tipo->nome}} - {{$tipo->descricao}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
 
-                                    <select id="tipo_id"  class="form-control" name="tipo_id" autocomplete="tipo_id" autofocus>
-                                        <option value="">Indefinido</option>
-                                        @foreach($tipos as $tipo)
-                                            <option value="{{$tipo->id}}">{{$tipo->nome}} - {{$tipo->descricao}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-12">
+                                            <label for="quantidade" >{{ __('Quantidade') }}</label>
 
-                            <div class="form-group row">
-                                <label for="quantidade" class="col-md-4 col-form-label text-md-right">{{ __('Quantidade') }}</label>
-                                <div class="col-md-6">
-                                    <input id="quantidade" type="quantidade" class="form-control @error('quantidade') is-invalid @enderror" name="quantidade" value="{{ sizeof(Session::get('lista')) }}" required autocomplete="quantidade">
+                                            <input id="quantidade" type="quantidade" class="form-control @error('quantidade') is-invalid @enderror" name="quantidade" value="{{ sizeof(Session::get('lista')) }}" required autocomplete="quantidade">
 
-                                    @error('quantidade')
-                                    <span class="invalid-feedback" role="alert">
+                                            @error('quantidade')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+
+                                        <div class="col-md-12">
+                                            <label for="numeros">{{ __('NumPatrimonio') }}</label>
+
+                                            <table>
+                                                <tr>
+                                                    <th>Numero</th>
+                                                </tr>
+                                                @foreach(Session::get('lista') as $i => $patrimonio)
+                                                    <tr>
+                                                        <td>{{$patrimonio}}</td>
+                                                        <td><a href="{{route('remover',['id' => $i])}}">Remover</a></td>
+                                                    </tr>
+                                                @endforeach
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Adicionar') }}
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
+                            <div class="col-md-6">
+                                <form method="POST" action="{{ route('anexar') }}">
+                                    @csrf
 
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('NumPatrimonio') }}</label>
+                                    <div class="form-group row">
+                                        <div class="col-md-12">
+                                            <label for="numero">{{ __('Numero') }}</label>
 
-                                <div class="col-md-6">
-                                    <table>
-                                        <tr>
-                                            <th>Numero</th>
-                                        </tr>
-                                        @foreach(Session::get('lista') as $i => $patrimonio)
-                                            <tr>
-                                                <td>{{$patrimonio}}</td>
-                                                <td><a href="{{route('remover',['id' => $i])}}">Remover</a></td>
-                                            </tr>
-                                        @endforeach
-                                    </table>
-                                </div>
+                                            <input id="numero" type="numero" class="form-control @error('numero') is-invalid @enderror" name="numero" value="{{ old('numero') }}" required autocomplete="quantidade">
+
+                                            @error('numero')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Anexar') }}
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Adicionar') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                        <br>
-                        <form method="POST" action="{{ route('anexar') }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="numero" class="col-md-4 col-form-label text-md-right">{{ __('Numero') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="numero" type="numero" class="form-control @error('numero') is-invalid @enderror" name="numero" value="{{ old('numero') }}" required autocomplete="quantidade">
-
-                                    @error('numero')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Anexar') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
